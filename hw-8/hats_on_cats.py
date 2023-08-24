c2038ff91a53e2e7de5892365f33eea087a838f7
Serhii Hrachov <bassdarkside@gmail.com>
2023-08-24 09:11:43 +0300

    modified: hats_on_cats.py

--- hw-8/hats_on_cats.py
+++ hw-8/hats_on_cats.py
@@ -1,15 +1,12 @@
+# Time complexity O(n^2)
 def loop(cats):
-    cats_on_hats_indices = [True] * (cats + 1)
-    count = 2
-    result = ()
-    while count <= cats:
-        for i in range(count, cats + 1, count):
-            cats_on_hats_indices[i] = not cats_on_hats_indices[i]
-        count += 1
-    for cat_index in range(1, cats + 1):
-        if cats_on_hats_indices[cat_index]:
-            result += (cat_index,)
-    return result
+    all_cats = [True] * (cats + 1)
+    for i in range(2, cats + 1):
+        for j in range(i, cats + 1, i):
+            all_cats[j] = not all_cats[j]
+    return [
+        cat_index for cat_index in range(1, cats + 1) if all_cats[cat_index]
+    ]
 
 
 cats_with_hats = loop(100)

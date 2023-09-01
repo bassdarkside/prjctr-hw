e145ec336f11abe9b4cd0288d841f2f9fe510b0b
Serhii Hrachov <bassdarkside@gmail.com>
2023-09-01 21:20:45 +0300

    removed: hw-10; modified: gif_search.py, gifler_bot.py

--- bot_telegram/gifler_bot.py
+++ bot_telegram/gifler_bot.py
@@ -1,17 +1,18 @@
 import requests
 import telebot
-from telebot import types
+from telebot import types, apihelper, logging
 from decouple import config
 
 
-GIPHY_KEY = config("GIPHY_KEY")
 BOT_TOKEN = config("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
 def gif_request(keyword):
+    GIPHY_KEY = config("GIPHY_KEY")
+    params = "&limit=1&offset=0&rating=r&lang=en"
     end_point = "http://api.giphy.com/v1/gifs/search"
-    url = f"{end_point}?q={keyword}&api_key={GIPHY_KEY}&limit=1&offset=0&rating=r"
+    url = f"{end_point}?q={keyword}&api_key={GIPHY_KEY}{params}"
     response = requests.get(url)
     return response.json()
 
@@ -31,7 +32,7 @@ def start(message):
     markup.add(btn1)
     bot.send_message(
         message.chat.id,
-        f"👋 Hi, {message.from_user.first_name}! I'm GiflerBot!",
+        f"👋 Hi, {message.from_user.first_name}! Let's get search!",
         reply_markup=markup,
     )
 
@@ -47,9 +48,15 @@ def get_text_messages(message):
         )
     elif message.text:
         list_results = build_output(message.text.lower())
+        bot.send_message(message.from_user.id, "Here's what I found for you 👉")
         bot.send_message(
             message.from_user.id, f"{list_results}", parse_mode="Markdown"
         )
 
 
-bot.polling(none_stop=True, interval=0)
+try:
+    apihelper.SESSION_TIME_TO_LIVE = 5 * 60
+    apihelper.RETRY_ON_ERROR = True
+    bot.infinity_polling()
+except telebot.apihelper.ApiTelegramException as e:
+    logging.log(e)
--- gif_search/gif_search.py
+++ gif_search/gif_search.py
@@ -3,28 +3,20 @@ from decouple import config
 from colorama import Fore
 
 
-KEY = config("KEY")
-
-B = Fore.BLUE
-G = Fore.GREEN
-Y = Fore.YELLOW
-
-
 def gif_request(keyword):
+    KEY = config("KEY")
+    B = Fore.BLUE
+    Y = Fore.YELLOW
     end_point = "http://api.giphy.com/v1/gifs/search"
     url = f"{end_point}?q={keyword}&api_key={KEY}&limit=5&offset=0&rating=r"
-    response = requests.get(url)
-    return response.json()
-
-
-def build_output(*args):
-    response = gif_request(*args)
-    res = ""
+    response = requests.get(url).json()
+    results = ""
     for gif in response["data"]:
-        res += f"{G}{gif['bitly_url']} <-- {B}{gif['title']}{Y}\n"
-    return res
+        results += f"{G}{gif['bitly_url']} <-- {B}{gif['title']}{Y}\n"
+    return results
 
 
+G = Fore.GREEN
 user_input = input(f"{G}Enter keyword for GIF search: {Fore.RESET}")
-list_results = build_output(user_input)
+list_results = gif_request(user_input)
 print(f"Search results:\n{list_results}")

13eab0fe31619998ffca2b8e23ebff70ca02b726
Serhii Hrachov <bassdarkside@gmail.com>
2023-09-03 10:32:40 +0300

    Revert "removed: hw-9"
    This reverts commit 170ab64ad0adaa679f1d4384665f8fc9dfb17ec4.

--- hw-10/create_and_copy.py
+++ hw-10/create_and_copy.py
@@ -20,6 +20,7 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
 
+
 filename = pathlib.Path("git_repo/hw-10/zen.txt")
 
 
---
+++ hw-9/main.py
@@ -0,0 +1,4 @@
+from hello import hello
+
+name = "Serhii"
+hello(name)

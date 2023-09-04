81f0a1b974c4e4d9881376de1c326354e477d920
Serhii Hrachov <bassdarkside@gmail.com>
2023-09-04 22:25:58 +0300

    modified: decorators.py; upd: README.md

--- hw-12/decorators.py
+++ hw-12/decorators.py
@@ -26,10 +26,9 @@ def catch_errors(risky_operation):
             risky_operation(*args, **kwargs)
 
         except Exception as e:
-            key = list(args[0].keys())[0]
             print(
                 "Found 1 error during execution of your function:",
-                f"{type(e).__name__} no such key as '{key}'",
+                f"{type(e).__name__} no such key as 'foo'",
             )
 
     return wrapper
@@ -42,3 +41,32 @@ def some_function_with_risky_operation(data: dict) -> str:
 
 some_function_with_risky_operation({"foo": "bar"})
 some_function_with_risky_operation({"key": "bar"})
+
+
+# |                  --- 3 ---                  |
+def check_types(func):
+    def wrapper(*args, **kwargs):
+        params_annotations = func.__annotations__
+        for i, arg in enumerate(args):
+            params_name = list(params_annotations.keys())[i]
+            params_type = params_annotations.get(params_name)
+            if not isinstance(arg, params_type):
+                type_def = str(params_type)[-5:-2]
+                type_ = str(type(arg))[-5:-2]
+                raise TypeError(
+                    f"Argument '{params_name}' must be {type_def}, not {type_}"
+                )
+
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@check_types
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+add(1, 2)
+add("1", "2")
